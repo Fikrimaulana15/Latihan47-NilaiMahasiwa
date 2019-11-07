cbd9c6d14e5772bf567149f20aa960556a85f4fa
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-11-07 19:14:10 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118074/latihan47/nilaimahasiswa/PBO210118074LATIHAN47NilaiMahasiswa.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihan47.nilaimahasiswa;
+
+/**
+ *
+ * @author ASUS
+ * NAMA : fikri maulana
+ * KElAS : if2
+ * NIM : 10118074
+ */
+public class PBO210118074LATIHAN47NilaiMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    NilaiAkhir na1,na2,na3;
+        
+        na1 = new NilaiAkhir(75, 45, 34);
+        na1.tampilNilai();
+        na2 = new NilaiAkhir(60, 80, 75);
+        na2.tampilNilai();
+        na3 = new NilaiAkhir(30, 80, 40);
+        na3.tampilNilai();
+        
+        System.out.println("developed by : fikri maulana");
+    }
+    
+}
+  
+
